890030a397a48dce3e7f8d999df64ce092046de8
Prasanna Dangalla <prasannadangalla@gmail.com>
2021-06-23 11:35:31 +0530

    Improved test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,5 +1,6 @@
-curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
-sudo yum install nodejs
+sudo apt-get update
+sudo apt-get install nodejs
+sudo apt-get install npm
 npm install cypress
 npm install --save-dev cypress-file-upload
-#npm run test
+npm run test
