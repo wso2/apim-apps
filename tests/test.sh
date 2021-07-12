dc5332b74c0093242daed4e06b9a21232fccdd8e
Prasanna Dangalla <prasannadangalla@gmail.com>
2021-07-12 16:50:39 +0530

    Modified test.sh.

--- tests/test.sh
+++ tests/test.sh
@@ -38,6 +38,6 @@ npm run report:merge
 npm run report:generate
 mv  ./cypress/reports/html/mochawesome-bundle.html  ./cypress/reports/html/mochawesome-bundle-${TEST_PLAN_ID}.html
 sleep 1500
-node ./upload_email
+#node ./upload_email
 cd ../scenarios
 mvn clean install
