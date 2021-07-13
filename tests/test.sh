d126d9a9cbeed61c3373e986b5c80598e2080595
Prasanna Dangalla <prasannadangalla@gmail.com>
2021-07-13 12:46:11 +0530

    Modified test.sh.

--- tests/test.sh
+++ tests/test.sh
@@ -92,7 +92,6 @@ sudo dpkg --configure -a
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt install -y npm
 sudo apt-get install -y nodejs
-sudo apt install nodejs-legacy
 npm install cypress
 npm install --save-dev cypress-file-upload
 npm install --save  nodemailer
