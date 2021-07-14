d2532d9c1cd25ea7fd7927deb51780dbb99a15dd
Prasanna Dangalla <prasannadangalla@gmail.com>
2021-07-14 10:14:22 +0530

    Modified test.sh.

--- tests/test.sh
+++ tests/test.sh
@@ -91,7 +91,8 @@ echo `pwd`
 
 echo "Copying surefire-reports to ${OUTPUT_DIR}/scenarios"
 mkdir -p ${OUTPUT_DIR}/scenarios
-echo "Sample" > ${OUTPUT_DIR}/scenarios/sample.txt
+mkdir -p ${OUTPUT_DIR}/scenarios/sample
+echo "Sample" > ${OUTPUT_DIR}/scenarios/sample/sample.txt
 find . -name "surefire-reports" -exec cp --parents -r {} ${OUTPUT_DIR}/scenarios \;
 find . -name "aggregate-surefire-report" -exec cp --parents -r {} ${OUTPUT_DIR}/scenarios \;
 
@@ -99,5 +100,6 @@ find . -name "aggregate-surefire-report" -exec cp --parents -r {} ${OUTPUT_DIR}/
 
 echo "Generating Scenario Code Coverage Reports"
 source ${HOME}/code-coverage/code-coverage.sh
-echo "Sample" > ${HOME}/code-coverage/sample.txt
+mkdir -p ${HOME}/code-coverage/sample
+echo "Sample" > ${HOME}/code-coverage/sample/sample.txt
 generate_code_coverage ${INPUT_DIR} ${OUTPUT_DIR}
