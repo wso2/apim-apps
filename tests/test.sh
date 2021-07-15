cc4dd6bbf13a4a3e904adfa90c33c9ce229b4fae
Prasanna Dangalla <prasannadangalla@gmail.com>
2021-07-15 18:17:18 +0530

    Added npm removal code if exists.

--- tests/test.sh
+++ tests/test.sh
@@ -91,7 +91,13 @@ sleep 300
 sudo killall apt apt-get dpkg
 sudo dpkg --configure -a
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt install -y npm
+npm -v
+if [[$$? -ne 0 ]]
+then
+    sudo apt-get remove nodejs npm
+else
+    sudo apt install -y npm
+fi
 sudo apt-get install -y nodejs
 npm install cypress
 npm install --save-dev cypress-file-upload
