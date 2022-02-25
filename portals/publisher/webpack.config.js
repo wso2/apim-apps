9854a98ad74da660ca5acb2cee5f431f8fde2012
Ashera Silva <asherasilva100@gmail.com>
2022-02-25 20:45:15 +0530

    [Publisher Portal] Replace react-hot-loader with react-refresh

--- portals/publisher/webpack.config.js
+++ portals/publisher/webpack.config.js
@@ -23,6 +23,7 @@ const ESLintPlugin = require('eslint-webpack-plugin');
 const webpack = require('webpack');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 const { clientRoutingBypass, devServerBefore } = require('./services/dev_proxy/auth_login.js');
 
 // When exporting a function, Current mode is received as the first arg to the function.
@@ -126,7 +127,7 @@ module.exports = (env, argv) => {
                     use: { loader: 'worker-loader' },
                 },
                 {
-                    test: /\.(js|jsx)$/,
+                    test: /\.(js|jsx|ts|tsx)$/,
                     exclude: [/node_modules\/(?!(@hapi)\/).*/, /coverage/],
                     use: [
                         {
@@ -213,6 +214,7 @@ module.exports = (env, argv) => {
                 const pres = Math.round(percentage * 100);
                 if (pres % 20 === 0) console.info(`${pres}%`, message, ...args); // To reduce log lines
             }),
+            new ReactRefreshWebpackPlugin(),
         ],
     };
     const isAnalysis = process.env && process.env.NODE_ENVS === 'analysis';
