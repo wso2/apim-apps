d2d2273a7adb8a65c947daba41894e49d7d4340e
Ashera Silva <asherasilva100@gmail.com>
2022-03-03 16:50:27 +0530

    Add missing react import

--- portals/publisher/source/src/app/components/Apis/Details/Policies/OperationButton.tsx
+++ portals/publisher/source/src/app/components/Apis/Details/Policies/OperationButton.tsx
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-import { FC } from 'react';
+import React, { FC } from 'react';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
