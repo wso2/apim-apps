f24949dc528ca4a6c69e7eb0bc4acd8c4d425c0b
Ashera Silva <asherasilva100@gmail.com>
2022-03-15 15:13:13 +0530

    Increase the operationPolicyCount to 500

--- portals/publisher/site/public/conf/settings.js
+++ portals/publisher/site/public/conf/settings.js
@@ -31,7 +31,7 @@ const AppConfig = {
             timeout: 4000, // Defines the timeout for the above periodical session status check
         },
         throttlingPolicyLimit: 80,
-        operationPolicyCount: 50,
+        operationPolicyCount: 500,
         propertyDisplaySuffix: '__display',
         loadDefaultLocales: true, // If false, Default en.json file will not be used/loaded in app.
         // loadDefaultLocales = false is good for performance but text overrides using the locale file will not work
