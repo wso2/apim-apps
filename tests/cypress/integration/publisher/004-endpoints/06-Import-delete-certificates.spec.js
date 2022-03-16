1618d26d682e08dcc7b260ba0a6f576e61345379
dulith <dulithcsenanayake@gmail.com>
2022-03-16 10:59:30 +0530

    Add import/delete certificates test

---
+++ tests/cypress/integration/publisher/004-endpoints/06-Import-delete-certificates.spec.js
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Utils from "@support/utils";
+
+describe("Endpoint testing", () => {
+    const publisher = 'publisher';
+    const password = 'test123';
+    const carbonUsername = 'admin';
+    const carbonPassword = 'admin';
+
+    before(function () {
+        cy.carbonLogin(carbonUsername, carbonPassword);
+        cy.addNewUser(publisher, ['Internal/publisher', 'Internal/creator', 'Internal/everyone'], password);
+        cy.loginToPublisher(publisher, password);
+    })
+
+    it.only("Add REST endpoints for production and sandbox endpoints with failover", () => {
+        const random_number = Math.floor(Date.now() / 1000);
+        const testAlias = `endpointCert`;
+        const endpoint = `https://petstore.swagger.io/v2/store/inventory/${random_number}`;
+
+        cy.createAPIWithoutEndpoint();
+        cy.get('#itest-api-details-api-config-acc').click();
+        cy.get('#left-menu-itemendpoints').click();
+        cy.get('[data-testid="http/restendpoint-add-btn"]').click();
+
+        // Add the prod and sandbox endpoints
+        cy.get('#production-endpoint-checkbox').click();
+        cy.get('#production_endpoints').focus().type(endpoint);
+
+        //Expanding the general config section
+        cy.get('#http-panel1bh-header').trigger('click');
+        cy.get('#certs-add-btn').click();
+
+        cy.get('#endpoint-certificate').click();
+        cy.get(`[data-value="${endpoint}"]`).click();
+
+        cy.get('#certificateAlias').click();
+        cy.get('#certificateAlias').type(testAlias);
+
+        // upload the cert
+        cy.get('[data-testid="cert-upload-btn"]').click();
+        const filepath = 'api_artifacts/sample.crt.pem';
+        cy.get('input[type="file"]').attachFile(filepath);
+        
+        // Click away
+        cy.get('#certificateAlias').click();
+
+        // Save the cert
+        cy.get('#upload-cert-save-btn').type(filepath);
+        cy.wait(1000);
+
+        // Save the endpoint
+        cy.get('#endpoint-save-btn').click();
+        cy.get('#endpoint-save-btn').then(function (el) {
+            // Check the values
+            cy.get('#http-panel1bh-header').trigger('click');
+            cy.get('#endpoint-cert-list').contains(testAlias).should('be.visible');
+        });
+
+        cy.get('#http-panel1bh-header').click({force:true});
+        cy.get('#delete-cert-btn').click({force:true});
+        cy.get('#delete-cert-confirm-btn').click();
+        cy.get('#endpoint-cert-list').contains(testAlias).should('not.exist');
+    });
+
+    after(function () {
+        // Test is done. Now delete the api
+        cy.get(`#itest-id-deleteapi-icon-button`).click({force:true});
+        cy.get(`#itest-id-deleteconf`).click();
+
+        cy.visit(`${Utils.getAppOrigin()}/carbon/user/user-mgt.jsp`);
+        cy.deleteUser(publisher);
+    })
+});
