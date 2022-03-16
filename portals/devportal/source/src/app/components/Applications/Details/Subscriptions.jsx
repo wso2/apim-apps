c196a30945bb79f4e09ba467f0bfbd76b9d96156
chamilaadhi <cka.adhi@gmail.com>
2022-03-16 19:16:48 +0530

    Add tier pending notification and edit message

--- portals/devportal/source/src/app/components/Applications/Details/Subscriptions.jsx
+++ portals/devportal/source/src/app/components/Applications/Details/Subscriptions.jsx
@@ -336,6 +336,12 @@ class Subscriptions extends React.Component {
                                 + 'approval.',
                             id: 'subscription.pending',
                         }));
+                    } else if (response.body.status === 'TIER_UPDATE_PENDING') {
+                        Alert.info(intl.formatMessage({
+                            defaultMessage: 'Your subscription update request has been submitted and is now awaiting '
+                                + 'approval.',
+                            id: 'subscription.tierPending',
+                        }));
                     } else {
                         Alert.info(intl.formatMessage({
                             id: 'Applications.Details.Subscriptions.subscription.successful',
