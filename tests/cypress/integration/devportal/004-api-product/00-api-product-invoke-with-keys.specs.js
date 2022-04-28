7d80348b5a4f7ee4cd6645420795344dcaa3ade1
Tharanidk <tharani.dayadhi@gmail.com>
2022-04-28 15:37:25 +0530

    oath key invoke

--- tests/cypress/integration/devportal/004-api-product/00-api-product-invoke-with-keys.specs.js
+++ tests/cypress/integration/devportal/004-api-product/00-api-product-invoke-with-keys.specs.js
@@ -23,9 +23,6 @@ describe("Invoke API Product", () => {
     const carbonUsername = 'admin';
     const carbonPassword = 'admin';
     const productName = 'pizzaShackProduct';
-    // const apiName = `anonymous${Math.floor(Math.random() * (100000 - 1 + 1) + 1)}`;
-    // const apiVersion = '1.0.6';
-    // const apiContext = `anonymous${Math.floor(Math.random() * (100000 - 1 + 1) + 1)}`;
     const appName = 'InvokeApiProduct' + Math.floor(Date.now() / 1000);
     const appDescription = 'Testing app ';
 
@@ -102,7 +99,7 @@ describe("Invoke API Product", () => {
                     cy.get('#left-menu-itemdeployments').click();
 
                     // Deploying
-                    cy.get('#deploy-btn',{ timeout: 30000 }).click({"force":true});
+                    cy.get('#deploy-btn',{ timeout: 30000 }).click({force:true});
                     
                     //Publish the api
                     cy.on('uncaught:exception', (err, runnable) => {
@@ -114,7 +111,7 @@ describe("Invoke API Product", () => {
                     // Publishing api product
                     cy.wait(2000);
                     cy.get('[data-testid="Publish-btn"]').click();
-                    
+
                     cy.logoutFromPublisher();
 
                     //Log into developer portal
@@ -146,7 +143,6 @@ describe("Invoke API Product", () => {
                         //test
                         cy.get('#gen-test-key').should('not.have.attr', 'disabled', { timeout: 30000 });
                         cy.get('#gen-test-key').click();
-
                         cy.intercept('**/generate-token').as('genToken');
                         cy.wait('@genToken');
 
@@ -164,48 +160,6 @@ describe("Invoke API Product", () => {
                         
                     });
 
-                    //Test with API Key
-                    cy.createApp(appName, appDescription);
-                    
-                    cy.get('#production-keys-apikey').click();
-                    // Generate with none option
-                    cy.get('#generate-key-btn').then(() => {
-                        cy.get('#generate-key-btn').click();
-                        cy.get('#generate-api-keys-btn').click();
-                    })
-                            
-                    //Subscription of APi Product
-                    cy.get('#left-menu-subscriptions').click();
-                    cy.get('#subscribe-api-btn').click();
-
-                    cy.get(`#policy-subscribe-btn-${uuidProduct}`).click();
-                    cy.get('#close-btn').click();
-                    cy.location('pathname').then((pathName) => {
-                        const pathSegments = pathName.split('/');
-                        const uuidApp = pathSegments[pathSegments.length - 2];
-
-                        cy.visit(`${Utils.getAppOrigin()}/devportal/apis/${uuidProduct}/test`);
-
-                        //test
-                        cy.get('#gen-test-key').should('not.have.attr', 'disabled', { timeout: 30000 });
-                        cy.get('#gen-test-key').click();
-
-                        cy.intercept('**/generate-token').as('genToken');
-                        cy.wait('@genToken');
-
-                        // Test console with Oath2 token
-                        cy.get('#operations-default-get_menu').click();
-                        cy.get('#operations-default-get_menu .try-out__btn').click();
-                        cy.get('#operations-default-get_menu button.execute').click();
-                        cy.wait(3000);
-                        cy.get('#operations-default-get_menu  td.response-col_status').contains('200').should('exist');
-
-                        cy.visit(`${Utils.getAppOrigin()}/devportal/applications`);
-                        cy.get(`#delete-${appName}-btn`, { timeout: 30000 });
-                        cy.get(`#delete-${appName}-btn`).click();
-                        cy.get(`#itest-confirm-application-delete`).click();
-                        
-                    });
 
                     cy.logoutFromDevportal();
 
