c84458ba93230363881ae90808d56267ec6e9817
Suneth Samarasinghe <imsuneth@gmail.com>
2022-09-06 12:08:47 +0530

    Add UI tests for API linter feature

---
+++ tests/cypress/integration/publisher/021-api-linter-feature/03-lint-when-importing-api-with-swagger-url.spec.js
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Utils from "@support/utils";
+
+describe("Upload api spec from the api definition page", () => {
+    const publisher = 'publisher';
+    const password = 'test123';
+    const carbonUsername = 'admin';
+    const carbonPassword = 'admin';
+    const apiName = 'newapi' + Math.floor(Date.now() / 1000);
+    const apiVersion = '1.0.0';
+
+    before(function () {
+        cy.carbonLogin(carbonUsername, carbonPassword);
+        cy.addNewUser(publisher, ['Internal/publisher', 'Internal/creator', 'Internal/everyone'], password);
+        cy.loginToPublisher(publisher, password);
+    })
+
+    it.only("Upload api spec from the api definition page", () => {
+        cy.createAPIByRestAPIDesign(apiName, apiVersion);
+        cy.get('#itest-api-details-api-config-acc').click();
+        cy.get('#left-menu-itemAPIdefinition').click();
+        cy.get('#import-definition-btn').click();
+        cy.get('#open-api-url-select-radio').click();
+
+        // provide the swagger url
+        cy.get('[data-testid="swagger-url-endpoint"]').type('https://petstore3.swagger.io/api/v3/openapi.json');
+        // go to the next step
+        cy.get('#url-validated', { timeout: 30000 });
+
+        // check linter results
+        cy.get('[data-testid="itest-id-linter-results"]').should('be.visible');
+
+    });
+
+    after(function () {
+        cy.logoutFromPublisher();
+        // Test is done. Now delete the api
+        cy.deleteApi(apiName, apiVersion);
+
+        cy.visit(`${Utils.getAppOrigin()}/carbon/user/user-mgt.jsp`);
+        cy.deleteUser(publisher);
+    })
+});
