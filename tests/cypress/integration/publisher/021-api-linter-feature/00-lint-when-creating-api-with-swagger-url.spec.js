e8d66086b593e32c49ab1982d2f77dd23d3536dc
Bathiya Ladduwahetty <bathiya@wso2.com>
2022-11-07 12:57:46 +0530

    UI Test: fix lint test 00

---
+++ tests/cypress/integration/publisher/021-api-linter-feature/00-lint-when-creating-api-with-swagger-url.spec.js
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import Utils from "@support/utils";
+import OpenAPIPage from "../../../support/pages/publisher/OpenAPIPage";
+
+describe("publisher-021-00 : Lint when creating API with swagger URL", () => {
+    const publisher = 'publisher';
+    const password = 'test123';
+    const carbonUsername = 'admin';
+    const carbonPassword = 'admin';
+    
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;
+    });
+
+    before(function () {
+        cy.loginToPublisher(publisher, password);
+        OpenAPIPage.waitUntillLoadingComponentsExit()
+    });
+
+    it("Lint when creating API with swagger URL", () => {
+        cy.visit(`${Utils.getAppOrigin()}/`+OpenAPIPage.getUrl());
+
+        // select the option from the menu item
+        OpenAPIPage.openAPIURLRadioButton().click
+
+        // provide the swagger url
+        OpenAPIPage.openAPIURLTextBox().type('https://petstore3.swagger.io/api/v3/openapi.json')
+        OpenAPIPage.waitUntilGetUrlValidatedDiv(30000)
+        
+        // check linter results
+
+        OpenAPIPage.linterResultDivBlock().should('be.visible');
+        OpenAPIPage.errorsToggleButton().contains("0")
+        OpenAPIPage.warningToggleButton().contains("16")
+        
+        // Verify detail warning messages is display after clik on warning toggle button
+        // since tab is not working to leave from the text box click twice
+        OpenAPIPage.warningToggleButton().click
+        OpenAPIPage.waitUntilGetUrlValidatedDiv(30000)
+        OpenAPIPage.warningToggleButton().click
+        cy.contains('Operation "description" must be present and non-empty string.')
+
+        
+    });
+
+    after(function () {
+        cy.logoutFromPublisher();
+    })
+})
---
+++ tests/cypress/support/pages/publisher/OpenAPIPage.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+import PublisherComonPage from './PublisherComonPage';
+
+class OpenAPIPage extends PublisherComonPage{
+    static getUrl(){
+        return 'publisher/apis/create/openapi';
+    }
+    static openAPIURLRadioButton(){
+        return cy.get('#open-api-url-select-radio')
+    }
+
+    // typing on div also works '[data-testid="swagger-url-endpoint"]'
+    static openAPIURLTextBox(){
+        return cy.get('input[id="outlined-full-width"]')
+    }
+   static  waitUntilGetUrlValidatedDiv(timeoutInMs){
+        return cy.get('#url-validated', { timeout: timeoutInMs })
+    }
+    static linterResultDivBlock(){
+        return cy.get('[data-testid="itest-id-linter-results"]')
+    }
+    static errorsToggleButton(){
+        return cy.get('div[data-testid="itest-id-linter-results"]>div>div>div>div>div>div>button[value="0"]')
+    }
+    static warningToggleButton(){
+        return cy.get('div[data-testid="itest-id-linter-results"]>div>div>div>div>div>div>button[value="1"]')
+    }
+}
+export default OpenAPIPage;
