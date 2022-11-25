824422ec7c6123800eff6c835798ab1cf4eaec85
Tharika Madurapperuma <tharikaGitHub@users.noreply.github.com>
2022-11-25 05:47:25 +0530

    Revert "Upgrade REST API version for 4.2.0"

--- portals/publisher/src/main/webapp/source/src/app/data/Utils.js
+++ portals/publisher/src/main/webapp/source/src/app/data/Utils.js
@@ -624,7 +624,7 @@ Utils.CONST = {
     LOGOUT_CALLBACK: '/services/auth/callback/logout',
     INTROSPECT: '/services/auth/introspect',
     SERVICE_CATALOG_SWAGGER_YAML: '/api/am/service-catalog/v1/oas.yaml',
-    SWAGGER_YAML: '/api/am/publisher/v4/swagger.yaml',
+    SWAGGER_YAML: '/api/am/publisher/v3/swagger.yaml',
     PROTOCOL: 'https://',
     API_CLIENT: 'apiClient',
     SERVICE_CATALOG_CLIENT: 'serviceCatalogClient',
