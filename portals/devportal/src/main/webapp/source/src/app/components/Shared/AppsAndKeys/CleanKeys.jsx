0f35441b884c511302aa3fc4e038b01be706ce4a
Prasanna Dangalla <prasannadangalla@gmail.com>
2023-08-11 12:25:22 +0530

    Added delete confirmatiion.

--- portals/devportal/src/main/webapp/source/src/app/components/Shared/AppsAndKeys/CleanKeys.jsx
+++ portals/devportal/src/main/webapp/source/src/app/components/Shared/AppsAndKeys/CleanKeys.jsx
@@ -19,27 +19,68 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 import { FormattedMessage } from 'react-intl';
 import DeleteIcon from '@material-ui/icons/Delete';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 function CleanKeys(props) {
+    const [open, setOpen] = React.useState(false);
     const {
         keyMappingId, keys, selectedTab, handleClickClean,
     } = props;
 
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    const handleRemove = () => {
+        handleClickClean(keyMappingId);
+    };
+
     return (
-        <Button
-            id='remove-generated-keys'
-            variant='outlined'
-            color='secondary'
-            startIcon={<DeleteIcon />}
-            onClick={() => handleClickClean(keyMappingId)}
-            disabled={!keys.get(selectedTab)
-                .supportedGrantTypes.includes('client_credentials')}
-        >
-            <FormattedMessage
-                id='Shared.AppsAndKeys.ViewKeys.remove.keys'
-                defaultMessage='Remove Keys'
-            />
-        </Button>
+        <div>
+            <Button
+                id='remove-generated-keys'
+                variant='outlined'
+                color='secondary'
+                startIcon={<DeleteIcon />}
+                onClick={handleClickOpen}
+                disabled={!keys.get(selectedTab)
+                    .supportedGrantTypes.includes('client_credentials')}
+            >
+                <FormattedMessage
+                    id='Shared.AppsAndKeys.ViewKeys.remove.keys'
+                    defaultMessage='Remove Keys'
+                />
+            </Button>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                aria-labelledby='alert-dialog-title'
+                aria-describedby='alert-dialog-description'
+            >
+                <DialogTitle id='alert-dialog-title'>Do you really want to remove keys?</DialogTitle>
+                <DialogContent>
+                    <DialogContentText id='alert-dialog-description'>
+                        This will remove only the key entries stored in devportal, gateway and will not remove the service proveder keys.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color='primary'>
+                        Cancel
+                    </Button>
+                    <Button onClick={handleRemove} color='primary' autoFocus>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     );
 }
 
