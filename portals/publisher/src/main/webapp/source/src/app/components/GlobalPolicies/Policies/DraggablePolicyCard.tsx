d51e5378cce1c5650b3ae7ca198f6b2c1f0eea7d
piyumaldk <piyumaldk@gmail.com>
2023-11-07 12:06:21 +0530

    Shared DraggablePolicyCard component

--- portals/publisher/src/main/webapp/source/src/app/components/GlobalPolicies/Policies/DraggablePolicyCard.tsx
+++ portals/publisher/src/main/webapp/source/src/app/components/GlobalPolicies/Policies/DraggablePolicyCard.tsx
@@ -33,6 +33,7 @@ import type { Policy } from './Types';
 import ViewPolicy from './ViewPolicy';
 import DeletePolicy from './DeletePolicy';
 
+/** Shared UI Component */
 const useStyles = makeStyles(() => ({
     policyCardText: {
         overflow: 'hidden',
@@ -58,27 +59,30 @@ const style: CSSProperties = {
     borderRadius: '0.3em',
 };
 
-interface DraggablePolicyCardProps {
+interface DraggablePolicyCardSharedProps {
     policyObj: Policy;
     showCopyIcon?: boolean;
     isLocalToAPI: boolean;
     fetchPolicies: () => void;
+    setHovered: any;
+    hovered: boolean;
+    handleViewPolicy: () => void;
+    dialogOpen: boolean;
+    handleViewPolicyClose: () => void;
 }
 
-/**
- * Renders a single draggable policy block.
- * @param {any} DraggablePolicyCardProps Input props from parent components.
- * @returns {TSX} Draggable Policy card UI.
- */
-const DraggablePolicyCard: React.FC<DraggablePolicyCardProps> = ({
+const DraggablePolicyCardShared: React.FC<DraggablePolicyCardSharedProps> = ({
     policyObj,
     showCopyIcon,
     isLocalToAPI,
     fetchPolicies,
+    setHovered,
+    hovered,
+    handleViewPolicy,
+    dialogOpen,
+    handleViewPolicyClose
 }) => {
     const classes = useStyles();
-    const [hovered, setHovered] = useState(false);
-    const [dialogOpen, setDialogOpen] = useState(false);
 
     const [{ isDragging }, drag] = useDrag(
         () => ({
@@ -103,14 +107,6 @@ const DraggablePolicyCard: React.FC<DraggablePolicyCardProps> = ({
         }),
         [isDragging],
     );
-
-    const handleViewPolicy = () => {
-        setDialogOpen(true);
-    };
-
-    const handleViewPolicyClose = () => {
-        setDialogOpen(false);
-    };
 
     return (
         <>
@@ -195,6 +191,53 @@ const DraggablePolicyCard: React.FC<DraggablePolicyCardProps> = ({
                 isLocalToAPI={isLocalToAPI}
             />
         </>
+    );
+}
+
+/** Shared UI Component ends */
+
+interface DraggablePolicyCardProps {
+    policyObj: Policy;
+    showCopyIcon?: boolean;
+    isLocalToAPI: boolean;
+    fetchPolicies: () => void;
+}
+
+/**
+ * Renders a single draggable policy block.
+ * @param {any} DraggablePolicyCardProps Input props from parent components.
+ * @returns {TSX} Draggable Policy card UI.
+ */
+const DraggablePolicyCard: React.FC<DraggablePolicyCardProps> = ({
+    policyObj,
+    showCopyIcon,
+    isLocalToAPI,
+    fetchPolicies,
+}) => {
+    const [hovered, setHovered] = useState(false);
+    const [dialogOpen, setDialogOpen] = useState(false);
+
+
+    const handleViewPolicy = () => {
+        setDialogOpen(true);
+    };
+
+    const handleViewPolicyClose = () => {
+        setDialogOpen(false);
+    };
+
+    return (
+        <DraggablePolicyCardShared
+            policyObj={policyObj}
+            showCopyIcon={showCopyIcon}
+            isLocalToAPI={isLocalToAPI}
+            fetchPolicies={fetchPolicies}
+            setHovered={setHovered}
+            hovered={hovered}
+            handleViewPolicy={handleViewPolicy}
+            dialogOpen={dialogOpen}
+            handleViewPolicyClose={handleViewPolicyClose}
+        />
     );
 };
 
