0e82ab6a3e50007a9f5d8c12a6f82de9cd0bba32
yasasrangika <rangikaymendis@gmail.com>
2023-12-17 03:04:51 +0530

    Fix issues in ui tests

--- tests/cypress/integration/publisher/022-gateway-policies/00-gateway-policy.spec.js
+++ tests/cypress/integration/publisher/022-gateway-policies/00-gateway-policy.spec.js
@@ -26,7 +26,7 @@ describe("Gateway Policies", () => {
 
     it("Global Policy Operations", () => {
         // Add policy mapping
-        cy.visit(`publisher/global-policies/create`);
+        cy.visit(`publisher/global-policies/create`, { timeout: Cypress.config().largeTimeout });
         cy.get('#outlined-required').click();
         cy.get('#outlined-required').type('Global policy mapping 1');
         cy.get('#outlined-multiline-static').click();
@@ -44,38 +44,48 @@ describe("Gateway Policies", () => {
         cy.get('#headerValue').click();
         cy.get('#headerValue').type('RequestHeaderValue');
         cy.get('[data-testid=policy-attached-details-save]').click();
-        cy.get('[data-testid=policy-mapping-save-or-update-button]').scrollIntoView().click();
+        cy.get('[data-testid=policy-mapping-save-or-update-button]')
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
         cy.wait(2000);
 
         // deploy policy mapping to a gateway
-        cy.get('#expandable-button').click();
-        cy.contains('Select Gateways to Deploy').click();
+        cy.visit(`publisher/global-policies`, { timeout: Cypress.config().largeTimeout });
+        cy.get('td button') // Get all buttons within <td> elements
+            .first() // Select the first button found
+            .click();
+        cy.get('input[placeholder="Select Gateways to Deploy"]').click();
         cy.contains('Default').click();
         cy.get('[data-testid=policy-mapping-deploy-button]').click();
         cy.get('[data-testid=deploy-to-gateway-button]', { timeout: Cypress.config().largeTimeout }).click();
 
         // update policy mapping
+        cy.visit(`publisher/global-policies`, { timeout: Cypress.config().largeTimeout });
         cy.get('[data-testid=policy-mapping-edit-button]').click();
         cy.get('div[title="Add Header : v1"]').click();
         cy.get('#headerValue', { timeout: Cypress.config().largeTimeout }).click();
-        cy.get('[data-testid=policy-attached-details-save]').should('be.disabled'); // Check that the save button is initially disabled
+        cy.get('[data-testid=policy-attached-details-save]').should('be.disabled');
         const newText = 'RequestHeaderValueNew';
-        cy.get('#headerValue').type(newText).should('have.value', newText); // Check that the save button becomes enabled after text input
+        cy.get('#headerValue').clear().type(newText).should('have.value', newText);
         cy.get('[data-testid=policy-attached-details-save]').should('not.be.disabled');
         cy.get('[data-testid=policy-attached-details-save]').click();
-        cy.get('[data-testid=policy-mapping-save-or-update-button]').scrollIntoView().click();
+        cy.get('[data-testid=policy-mapping-save-or-update-button]')
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
+        cy.wait(2000);
 
         // undeploy policy mapping
-        cy.get(`#gateway-chip-${gateway}`)
-            .should('exist')
-            .then(($element) => {
-                if ($element.length > 0) {
-                    cy.wrap($element).click();
-                    cy.get('[data-testid=undeploy-from-gateway-button]', { timeout: Cypress.config().largeTimeout }).click();
-                }
-            });
+        cy.visit(`publisher/global-policies`, { timeout: Cypress.config().largeTimeout });
+        const gateway = 'Default';
+        cy.get(`#gateway-chip-${gateway} svg[class*=MuiChip-deleteIcon]`)
+            .should('be.visible')
+            .click();
+        cy.get('[data-testid=undeploy-from-gateway-button]', { timeout: Cypress.config().largeTimeout }).click();
 
-        //successfull deletion of policy mapping
+        //Successful deletion of policy mapping
+        cy.visit(`publisher/global-policies`, { timeout: Cypress.config().largeTimeout });
         cy.get('[data-testid=policy-mapping-delete-button]').click();
         cy.get('[data-testid=policy-mapping-delete-confirmation-button]', { timeout: Cypress.config().largeTimeout }).click();
 
