2059e4e22d63617a907202a797df746c1416d93f
Thisal Tennakoon <thisaltennakoon@gmail.com>
2024-02-05 18:13:03 +0530

    Code refine

--- portals/devportal/src/main/webapp/source/Tests/setupTests.js
+++ portals/devportal/src/main/webapp/source/Tests/setupTests.js
@@ -16,11 +16,8 @@
  * under the License.
  */
 import React from 'react';
-import Enzyme, { shallow, render, mount } from 'enzyme';
-// import Adapter from 'enzyme-adapter-react-17-updated';
+import { shallow, render, mount } from 'enzyme';
 import renderer from 'react-test-renderer';
-// React 16 Enzyme adapter
-// Enzyme.configure({ adapter: new Adapter() });
 
 // Make Enzyme functions available in all test files without importing
 global.React = React;
