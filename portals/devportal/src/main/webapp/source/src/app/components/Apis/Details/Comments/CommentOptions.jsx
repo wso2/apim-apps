7fcbd7ede2c0efb9e337c3ff6521353a2fc4270d
Thisal Tennakoon <thisaltennakoon@gmail.com>
2024-02-05 16:04:29 +0530

    Fixed nested commenting issue

--- portals/devportal/src/main/webapp/source/src/app/components/Apis/Details/Comments/CommentOptions.jsx
+++ portals/devportal/src/main/webapp/source/src/app/components/Apis/Details/Comments/CommentOptions.jsx
@@ -25,7 +25,7 @@ import AuthManager from '../../../../data/AuthManager';
 
 const styles = theme => ({
     link: {
-        color: theme.palette.getContrastText(theme.palette.primary.main),
+        fontWeight: 400,
         '& span.MuiButton-label': {
             color: theme.palette.primary.main,
             fontWeight: '400',
