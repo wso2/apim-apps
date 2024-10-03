7c3b31b2e1a4d9187f8fb99448b6dc38097a56ff
Avishka-Shamendra <avishka199849@gmail.com>
2024-10-03 15:41:50 +0530

    Fix publisher tasks page UI

--- portals/publisher/src/main/webapp/source/src/app/components/Base/Header/navbar/GlobalNavBar.jsx
+++ portals/publisher/src/main/webapp/source/src/app/components/Base/Header/navbar/GlobalNavBar.jsx
@@ -94,9 +94,11 @@ const GlobalNavBar = (props) => {
 
     let isRootPage = false;
     const { pathname } = location;
-    if (/^\/(apis|subscription|api-products|scopes|policies|global-policies|service-catalog)($|\/$)/g.test(pathname)) {
+
+    /* eslint-disable max-len */
+    if (/^\/(apis|subscription(\/.*)?|api-products|scopes|policies|global-policies|service-catalog)($|\/$)/g.test(pathname)) {
         isRootPage = true;
-    }
+    }    
     useEffect(() => {
         if (!isRootPage) {
             setOpen(false);
