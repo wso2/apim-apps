ce0928527222b099e1053d2557dbe8aa5766d241
Sachini De Silva <madusha.sachini@yahoo.com>
2025-03-26 12:40:32 +0530

    Fix issues with custom gateway agents

--- portals/publisher/src/main/webapp/source/src/app/components/Apis/Create/Components/DefaultAPIForm.jsx
+++ portals/publisher/src/main/webapp/source/src/app/components/Apis/Create/Components/DefaultAPIForm.jsx
@@ -186,6 +186,15 @@ export default function DefaultAPIForm(props) {
             && (Boolean(api.version))
             && Boolean(api.context));
 
+        // If the gateway type is not in the gatewayTypeMap, add it with both key and value equal to the type
+        if (settings.gatewayTypes) {
+            settings.gatewayTypes.forEach(type => {
+                if (!(type in gatewayTypeMap)) {
+                    gatewayTypeMap[type] = type;
+                }
+            });
+        }
+
         if (multiGateway) {
             const settingsEnvList = settings && settings.environment;
             multiGateway.forEach((gateway) => {
