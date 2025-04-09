f3557eeb6dd1c65fc061a23373a282d1374c32ae
Ashan Rathnaweera <ashanahr@gmail.com>
2025-04-09 11:25:54 +0530

    Allow the organization reference ID to be editable

--- portals/admin/src/main/webapp/source/src/app/components/Organizations/AddEditOrganization.jsx
+++ portals/admin/src/main/webapp/source/src/app/components/Organizations/AddEditOrganization.jsx
@@ -76,7 +76,6 @@ function AddEditOrganization(props) {
         description: '',
     });
     const [state, dispatch] = useReducer(reducer, initialState);
-    const [editMode, setIsEditMode] = useState(false);
     const {
         referenceId, displayName, description,
     } = state;
@@ -185,7 +184,7 @@ function AddEditOrganization(props) {
 
         const promiseAPICall = dataRow
             ? new API().updateOrganization(
-                dataRow.externalOrganizationId,
+                referenceId,
                 dataRow.organizationId,
                 displayName,
                 description,
@@ -227,7 +226,6 @@ function AddEditOrganization(props) {
                 displayName: originalName,
                 description: originalDescription,
             } = dataRow;
-            setIsEditMode(true);
             dispatch({
                 field: 'editDetails',
                 value: {
@@ -293,7 +291,6 @@ function AddEditOrganization(props) {
                         fullWidth
                         error={hasErrors('referenceId', referenceId)}
                         variant='outlined'
-                        disabled={editMode}
                     />
                     <Tooltip
                         title={(
