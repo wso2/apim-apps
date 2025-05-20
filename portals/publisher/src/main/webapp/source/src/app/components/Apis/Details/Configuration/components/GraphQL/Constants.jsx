f4d22ed32d157581b2e2804bb369ed8f2a691205
Irash-Perera <irash.21@cse.mrt.ac.lk>
2025-05-21 00:43:56 +0530

    Update copyright information

--- portals/publisher/src/main/webapp/source/src/app/components/Apis/Details/Configuration/components/GraphQL/Constants.jsx
+++ portals/publisher/src/main/webapp/source/src/app/components/Apis/Details/Configuration/components/GraphQL/Constants.jsx
@@ -1,7 +1,7 @@
 /*
- * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2025, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
  *
- * WSO2 Inc. licenses this file to you under the Apache License,
+ * WSO2 LLC. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
@@ -15,6 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+
 // Constants and configuration data for GraphQL Query Analysis
 export const PREFIX = 'GraphQLQueryAnalysis';
 export const API_ENDPOINT = 'http://127.0.0.1:8000';
