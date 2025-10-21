228694b635a8be6a7507c2ca2b33c0901d622a4f
dharam <dharmendra.20208039@mnnit.ac.in>
2025-10-21 12:56:34 +0530

    fix: add maxWidth constraint to Comments component to prevent horizontal overflow
    - Added maxWidth: theme.custom.contentAreaWidth to contentWrapper class
    - This ensures Comments page respects the same width constraints as other pages
    - Fixes horizontal scrolling issue reported in PR feedback

--- portals/publisher/src/main/webapp/source/src/app/components/Apis/Details/Comments/Comments.jsx
+++ portals/publisher/src/main/webapp/source/src/app/components/Apis/Details/Comments/Comments.jsx
@@ -69,6 +69,7 @@ const Root = styled('div')((
     [`&.${classes.contentWrapper}`]: {
         paddingLeft: theme.spacing(3),
         marginTop: theme.spacing(1),
+        maxWidth: theme.custom.contentAreaWidth,
         '& span, & h5, & label, & td, & li, & div, & input': {
             color: theme.palette.getContrastText(theme.palette.background.paper),
         },
