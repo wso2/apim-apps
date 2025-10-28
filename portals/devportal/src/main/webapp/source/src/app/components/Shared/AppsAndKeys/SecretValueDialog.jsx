d21041d4b5fffafc80f6d86a48927b7140634374
hisanhunais <hisan.live@gmail.com>
2025-10-28 15:01:18 +0530

    Remove unnecessary code segments

--- portals/devportal/src/main/webapp/source/src/app/components/Shared/AppsAndKeys/SecretValueDialog.jsx
+++ portals/devportal/src/main/webapp/source/src/app/components/Shared/AppsAndKeys/SecretValueDialog.jsx
@@ -10,23 +10,12 @@ import {
     Tooltip,
     Box,
 } from "@mui/material";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import CheckIcon from "@mui/icons-material/Check";
 
 import ViewSecret from "./ViewSecret"; // your existing component
 
 export default function SecretValueDialog({ open, onClose, secret }) {
-    const [copied, setCopied] = React.useState(false);
-    console.log("secret");
-    console.log(secret);
 
     if (!secret) return null; // nothing to show
-
-    const handleCopy = () => {
-        navigator.clipboard.writeText(secret.secretValue || "");
-        setCopied(true);
-        setTimeout(() => setCopied(false), 1500);
-    };
 
     return (
         <Dialog
@@ -52,13 +41,6 @@ export default function SecretValueDialog({ open, onClose, secret }) {
                     pb: 2,
                 }}
             >
-                {/* <Box>
-                    <Tooltip title={copied ? "Copied!" : "Copy Secret Value"}>
-                        <IconButton onClick={handleCopy} color={copied ? "success" : "default"}>
-                            {copied ? <CheckIcon /> : <ContentCopyIcon />}
-                        </IconButton>
-                    </Tooltip>
-                </Box> */}
                 <Button onClick={onClose} variant="contained" color="primary">
                     Close
                 </Button>
