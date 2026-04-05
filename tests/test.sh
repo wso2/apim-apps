ca07da610b85536ff9bbf6375dc8f0a341e8c512
ashiduDissanayake <ashidudissanayake1@gmail.com>
2026-04-05 23:52:32 +0530

    Update CI pipeline Node versions for Cypress 15 compatibility
    - ui-test.yml: Change Node 18 to 22 and cypress-io/github-action v4 to v6
    - test.sh: Update Node 12.22.3 to 22.22.1 for Cypress 15 support

--- tests/test.sh
+++ tests/test.sh
@@ -100,7 +100,7 @@ sudo killall apt apt-get dpkg
 sudo dpkg --configure -a
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | apt-key add -
 chmod 644 /usr/share/keyrings/nodesource.gpg
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_22.x | sudo -E bash -
 npm -v
 if [[ $? -ne 0 ]]
 then
@@ -111,11 +111,11 @@ else
     echo "NPM Deos NOT exists and installing existing version."
 fi
 
-wget https://nodejs.org/dist/v12.22.3/node-v12.22.3-linux-x64.tar.xz
-tar -xvf node-v12.22.3-linux-x64.tar.xz
-sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/node /usr/bin/node
-sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/npm /usr/bin/npm
-sudo ln -s $HOME/node-v12.22.3-linux-x64/bin/npx /usr/bin/npx
+wget https://nodejs.org/dist/v22.22.1/node-v22.22.1-linux-x64.tar.xz
+tar -xvf node-v22.22.1-linux-x64.tar.xz
+sudo ln -sf $HOME/node-v22.22.1-linux-x64/bin/node /usr/bin/node
+sudo ln -sf $HOME/node-v22.22.1-linux-x64/bin/npm /usr/bin/npm
+sudo ln -sf $HOME/node-v22.22.1-linux-x64/bin/npx /usr/bin/npx
 sudo apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb -y
 export LC_CTYPE="en_US.UTF-8"
 cd $HOME
